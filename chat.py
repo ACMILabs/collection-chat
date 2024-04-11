b790e124737cee4b4de73ddf6c86f5adcaf951f3
Simon Loffler <simon.loffler@acmi.net.au>
2024-04-11 14:08:33 +0930

    #1 Add the ability to use a chat history to include in follow up questions

--- chat.py
+++ chat.py
@@ -19,11 +19,15 @@ from langchain.chains import create_qa_with_sources_chain
 from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain, LLMChain
+from langchain.memory import ConversationBufferMemory
 
 
-COLLECTION_NAME = 'works'
-PERSIST_DIRECTORY = 'works_db'
-REBUILD = False
+DATABASE_PATH = os.getenv('DATABASE_PATH', '')
+COLLECTION_NAME = os.getenv('COLLECTION_NAME', 'works')
+PERSIST_DIRECTORY = os.getenv('PERSIST_DIRECTORY', 'works_db')
+REBUILD = os.getenv('REBUILD', 'false').lower() == 'true'
+HISTORY = os.getenv('HISTORY', 'true').lower() == 'true'
 
 # Set true if you'd like langchain tracing via LangSmith https://smith.langchain.com
 os.environ['LANGCHAIN_TRACING_V2'] = 'false'
@@ -32,7 +36,7 @@ embeddings = OpenAIEmbeddings()
 docsearch = Chroma(
     collection_name=COLLECTION_NAME,
     embedding_function=embeddings,
-    persist_directory=PERSIST_DIRECTORY,
+    persist_directory=f'{DATABASE_PATH}{PERSIST_DIRECTORY}',
 )
 
 if len(docsearch) < 1 or REBUILD:
@@ -83,6 +87,28 @@ retrieval_qa = RetrievalQA(
     combine_documents_chain=final_qa_chain,
 )
 
+if HISTORY:
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    _template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.\
+    Make sure to avoid using any unclear pronouns.
+
+    Chat History:
+    {chat_history}
+    Follow Up Input: {question}
+    Standalone question:"""
+    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+    condense_question_chain = LLMChain(
+        llm=llm,
+        prompt=CONDENSE_QUESTION_PROMPT,
+    )
+
+    retrieval_qa = ConversationalRetrievalChain(
+        question_generator=condense_question_chain,
+        retriever=docsearch.as_retriever(),
+        memory=memory,
+        combine_docs_chain=final_qa_chain,
+    )
+
 print('=========================')
 print('ACMI collection chat v0.1')
 print('=========================\n')
@@ -90,12 +116,15 @@ print('=========================\n')
 while True:
     try:
         query = input('Question: ')
-        response = retrieval_qa.invoke(query)
+        if HISTORY:
+            response = retrieval_qa.invoke({'question': query}).get('answer')
+        else:
+            response = retrieval_qa.invoke(query).get('result')
         try:
-            print(f'Answer: {json.loads(response["result"])["answer"]}')
-            print(f'Sources: {json.loads(response["result"])["sources"]}\n\n')
+            print(f'Answer: {json.loads(response)["answer"]}')
+            print(f'Sources: {json.loads(response)["sources"]}\n')
         except TypeError:
-            print(f'Answer: {response}')
+            print(f'Answer: {response}\n')
     except KeyboardInterrupt:
-        print('\nNice chatting to you.\n')
+        print('\n\nNice chatting to you.\n')
         break
