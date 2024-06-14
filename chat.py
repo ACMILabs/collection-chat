e5702bed4a2270b76a689699b69d045a664f4bb6
Simon Loffler <simon.loffler@acmi.net.au>
2024-06-14 11:07:29 +0930

    Update chat history prompt with the latest LangChain best practice

--- chat.py
+++ chat.py
@@ -140,9 +140,11 @@ retrieval_qa = RetrievalQA(
 
 if HISTORY:
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-    TEMPLATE = """Given the following conversation and a follow up question, rephrase the \
-    follow up question to be a standalone question, in its original language.\
-    Make sure to avoid using any unclear pronouns.
+    TEMPLATE = """Given a chat history and the latest user question
+    which might reference context in the chat history,
+    formulate a standalone question which can be understood
+    without the chat history. Do NOT answer the question,
+    just reformulate it if needed and otherwise return it as is.
 
     Chat History:
     {chat_history}
